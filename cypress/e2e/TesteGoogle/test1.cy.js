1e4454c3ac8d59a9b4701424fcdca49fc5a6b431
DESKTOP-P4OHQOB\kaw <mirela.dima29782@gmail.com>
2023-03-27 20:51:05 +0300

    Iniial commit

---
+++ cypress/e2e/TesteGoogle/test1.cy.js
@@ -0,0 +1,12 @@
+describe('Siteul Google.com', () => {
+    it('functioneaza cu o cautare basic', ()=>{
+        cy.visit('http://google.com');
+        cy.get('#L2AGLb').click();
+        cy.get('.gLFyf').type('vlog de it').type('{enter}');
+
+        cy.get('#result-stats').should('exist');
+    })
+})
+
+
+
---
+++ cypress/e2e/TesteLinkedIn/teste-linkedin.cy.js
@@ -0,0 +1,18 @@
+describe('On LinkedIn', () =>{
+    it("I can login", () =>{
+        cy.visit("https://linkedin.com");
+
+        cy.get('[action-type="ACCEPT"]').click();
+    
+        cy.get('.nav__button-secondary').click();
+
+        cy.get('#username').type('mirela.dima29782@gmail.com');
+
+        cy.get('#password').type('y386842t');
+
+        cy.get('.btn__primary--large').click();
+
+        cy.get('#ember15').should('exist');
+    
+    })
+})
