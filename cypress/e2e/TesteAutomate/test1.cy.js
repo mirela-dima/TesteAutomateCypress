ca3483e62412eaeff8d9d8fc8b3fc6fadb0e7b77
mirela-dima <124343975+mirela-dima@users.noreply.github.com>
2023-05-15 11:50:44 +0300

    Update test1.cy.js

--- cypress/e2e/TesteAutomate/test1.cy.js
+++ cypress/e2e/TesteAutomate/test1.cy.js
@@ -3,13 +3,11 @@ describe('Siteul Libris.ro', () => {
     cy.visit('https://www.libris.ro/auth/login.jsp');
     cy.wait(8000);
     cy.get('.modal-form-newsletter-img-wr > .modal-content').type('{esc}');
-
     
-     cy.get('.log-in-cont-form-ct');
+    cy.get('.log-in-cont-form-ct');
     cy.get('#date-inregistrare-user').type('mirela.dima1982@yahoo.it');
     cy.get('#parola').type('aaaaaa');
     cy.get('#parola').type('{enter}');
     cy.get('.log-in-cont-form-date-inregistrare-ct > :nth-child(2)').should('exist');
-    
-})
+    })
 })
